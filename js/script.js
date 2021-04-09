a1cabb787e71433ca81e1aeeeb1bc00649079e44
Benito <bbxblack@gmail.com>
2021-04-09 15:07:17 +0200

    v0.1

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function initVue() {
       contacts: [
         {
           name: 'Michele',
-          avatar: '_1',
+          avatar: 'img/UT_1.jpg',
           visible: true,
           messages: [
             {
@@ -25,8 +25,73 @@ function initVue() {
               status: 'received'
             }
           ],
-          }
+        },
+        {
+            name: 'Fabio',
+            avatar: 'img/UT_2.jpg',
+            visible: true,
+            messages: [
+                {
+                    date: '20/03/2020 16:30:00',
+                    text: 'Ciao come stai?',
+                    status: 'sent'
+                },
+                {
+                    date: '20/03/2020 16:30:55',
+                    text: 'Bene grazie! Stasera ci vediamo?',
+                    status: 'received'
+                },
+                {
+                    date: '20/03/2020 16:35:00',
+                    text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                    status: 'sent'
+                }
+            ],
+        },
+        {
+            name: 'Samuele',
+            avatar: 'img/UT_3.jpg',
+            visible: true,
+            messages: [
+                {
+                    date: '28/03/2020 10:10:40',
+                    text: 'La Marianna va in campagna',
+                    status: 'received'
+                },
+                {
+                    date: '28/03/2020 10:20:10',
+                    text: 'Sicuro di non aver sbagliato chat?',
+                    status: 'sent'
+                },
+                {
+                    date: '28/03/2020 16:15:22',
+                    text: 'Ah scusa!',
+                    status: 'received'
+                }
+            ],
+        },
+        {
+            name: 'Luisa',
+            avatar: 'img/UT_3.jpg',
+            visible: true,
+            messages: [
+                {
+                    date: '10/01/2020 15:30:55',
+                    text: 'Lo sai che ha aperto una nuova pizzeria?',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 15:50:00',
+                    text: 'Si, ma preferirei andare al cinema',
+                    status: 'received'
+                }
+            ],
+        },
+    ];
     },
+    methods: {
+
+    }
 
     // creare le funzioni
   });
